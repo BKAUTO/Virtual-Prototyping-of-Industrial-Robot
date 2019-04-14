3fc5176cb5a79883fece87005e0275599c46b73b
WBK1847764699 <wubingkunwubingkun@163.com>
2019-04-14 22:22:10 +0800

    4/14-14

---
+++ Virtual Factory/Assets/Scripts/Rotate/Rotate4.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Chat;
+
+public class Rotate4 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Chat.ChatClient.variable[0] = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetKeyDown(KeyCode.E))
+            //transform.Rotate(new Vector3(0, 0, 10), Space.Self);
+        //else
+            transform.localEulerAngles = new Vector3(0, 0, -server.Demo.variable[0]);
+    }
+}
