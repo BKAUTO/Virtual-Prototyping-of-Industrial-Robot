4d33e7c8f8662f5efea252ebba89c07f77ecaf1f
WBK1847764699 <wubingkunwubingkun@163.com>
2019-04-14 22:17:46 +0800

    4/14-13

---
+++ Virtual Factory/Assets/Scripts/AngleDisplay/Angle2Display.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Angle2Display : MonoBehaviour
+{
+    public Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = server.Demo.variable[1].ToString();
+    }
+}
---
+++ Virtual Factory/Assets/Scripts/ChatControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+    public class ChatControl : MonoBehaviour
+    {
+
+        public InputField chatInput;
+        public Text chatText;
+        public ScrollRect scrollRect;
+        string username = "用户";
+        // Use this for initialization
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                if (chatInput.text != "")
+                {
+                    string addText = "\n  " + "<color=red>" + username + "</color>: " + chatInput.text;
+                    chatText.text += addText;
+                    chatInput.text = "";
+                    chatInput.ActivateInputField();
+                    Canvas.ForceUpdateCanvases();       //关键代码
+                    scrollRect.verticalNormalizedPosition = 0f;  //关键代码
+                    Canvas.ForceUpdateCanvases();   //关键代码
+                }
+            }
+
+        }
+    }
