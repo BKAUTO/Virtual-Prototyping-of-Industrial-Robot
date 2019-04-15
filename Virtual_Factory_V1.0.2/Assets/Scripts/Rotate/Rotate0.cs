a9e097e2751b205f8b4fc54d6a5e21898efb457d
WBK1847764699 <wubingkunwubingkun@163.com>
2019-04-15 22:19:30 +0800

    4_15_Assets

---
+++ Virtual_Factory_V1.0.2/Assets/Scripts/Rotate/Rotate0.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Chat;
+
+public class Rotate0 : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        Chat.ChatClient.variable[4] = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetKeyDown(KeyCode.A))
+            //transform.Rotate(new Vector3(0, 10, 0), Space.Self);
+        //else
+            transform.localEulerAngles = new Vector3(0, -server.Demo.variable[4], 0);
+    }
+}
---
+++ Virtual_Factory_V1.0.2/Assets/Scripts/Rotate/Rotate_Finger1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Finger1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.J))
+            transform.Rotate(new Vector3(0, 10, 0), Space.Self);
+    }
+}
