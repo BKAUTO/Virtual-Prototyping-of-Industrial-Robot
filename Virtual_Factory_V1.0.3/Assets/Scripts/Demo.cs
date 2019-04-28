67c1ae833d7430e355fd0cf5345953604f9f6dea
WBK1847764699 <wubingkunwubingkun@163.com>
2019-04-28 13:15:11 +0800

    Remote Control Realized
    Still got some problem in data receipt but remote control is good.

--- Virtual_Factory_V1.0.3/Assets/Scripts/Demo.cs
+++ Virtual_Factory_V1.0.3/Assets/Scripts/Demo.cs
@@ -17,24 +17,26 @@ namespace server
         public static string recvStr = "";//接收机械臂关节数据
         public static string recvStr2 = "";//机械臂控制信号
         public static byte[] recvBytes = new byte[1024];//机械臂关节数据内存缓冲区,1k
-        public static byte[] recvBytes2 = new byte[1024];//其他设备内存缓冲区
-        public static byte[] recvBytes3 = new byte[1024];//机械臂控制信号内存缓冲区
+        public static byte[] recvBytes2 = new byte[1024];//机械臂控制信号内存缓冲区
+        public static byte[] recvBytes3 = new byte[1024];//其他设备内存缓冲区
         public static int bytes1;//接收机械臂数据
-        public static int bytes2;//接收其他数据
-        public static int bytes3;//接收机械臂控制信号
+        public static int bytes2;//接收机械臂控制信号
+        public static int bytes3;//接收其他数据
         public static float[] variable = new float[6];
-        private const int port = 8088;
+        private const int port1 = 8088;
+        private const int port2 = 8087;
         //private static string host = "127.0.0.1";
-        private static Socket s;
-        public static Socket temp;
+        private static Socket s1,s2;
+        public static Socket temp1,temp2;
         public static bool flag = false, flag2 = false, flag3 = false;//flag用来指示机械臂关节数据传输socket连接情况，flag2控制前端提示信息显示，flag3指示外设（电爪）连接情况
         public static bool flag4 = false;//flag4指示机械臂控制信号socket连接情况
+        public static bool flag5 = false;//flag5指示是否可以发送控制信号给机械臂
         public Text chatText;
         public ScrollRect scrollRect;
         public static string displayText;
         public static Socket[] lst = new Socket[10];
-        private static int armSocketCount = 0;//机械臂创建两个socket客户端，一个用来发送关节角度给上位机，一个用来接收控制信号
-        public static double[] pose = new double[6] {295.157246157,-239.277773036,380.116285186,0.0,0.0,0.0};
+        private static int armSocketCount = 0;//机械臂创建两个socket客户端，一个用来发送关节角度给上位机，一个用来接收控制信号及发送控制数据，使用两个端口
+        public static double[] pose = new double[7] {295.157246157,-239.277773036,380.116285186,-0.01,0.7,0.05,0.6};
 
         public void TextPrint(string addText)
         {
@@ -53,41 +55,59 @@ namespace server
             return vari;
         }
 
-        private void ClientConnectListen()
+        private void ClientConnectListen1()
         {
             while (true)
             {
-                temp = s.Accept();
-                if (temp.RemoteEndPoint.ToString().Contains("192.168.2.160"))//机械臂连接上
+                temp1 = s1.Accept();
+                if (temp1.RemoteEndPoint.ToString().Contains("192.168.2.160"))//机械臂连接上
                 {
                    if(armSocketCount == 0)//机械臂数据传输socket
                     {
-                        lst[0] = temp;
+                        lst[0] = temp1;
                         flag = true;
                         armSocketCount += 1;
                         Debug.Log("关节数据socket连接成功");
-                    }
-                   else if(armSocketCount == 1)//机械臂关节控制信号socket
-                    {
-                        lst[2] = temp;
-                        flag4 = true;
-                        Debug.Log("远程控制socket连接成功");
                     }
                 }
                 else if(flag == false)//没有连接机械臂而先连接了其他设备
                 {
                     Debug.Log("2222");
-                    displayText = "\n" + "<color=red>" + "请先连接机械臂,程序出错请重启" + "</color>" + "\n";
+                    displayText = "\n" + "<color=red>" + "请先连接机械臂关节数据通道,程序出错请重启" + "</color>" + "\n";
                     flag2 = true;
-                    temp.Close();
+                    temp1.Close();
                 }
                 else//其他设备连接上（电爪）
                 {
                     Debug.Log("3333");
                     flag3 = true;
-                    lst[3]=temp;
+                    lst[3]=temp1;
                 }
-                displayText = "\n" + "<color=black>" + "客户端 " + "</color>" + "<color=green>" + temp.RemoteEndPoint.ToString() + "</color>" + "<color=black>" + "已连接\n" + "</color>";
+                displayText = "\n" + "<color=black>" + "客户端 " + "</color>" + "<color=green>" + temp1.RemoteEndPoint.ToString() + "</color>" + "<color=black>" + "已连接\n" + "</color>";
+                flag2 = true;
+            }
+        }
+
+        private void ClientConnectListen2()
+        {
+            while (true)
+            {
+                temp2 = s2.Accept();
+                if (temp2.RemoteEndPoint.ToString().Contains("192.168.2.160"))//机械臂连接上
+                {
+                    if(armSocketCount == 1)//机械臂关节控制信号socket
+                     {
+                         lst[2] = temp2;
+                         flag4 = true;
+                         Debug.Log("远程控制socket连接成功");
+                     }
+                    if(armSocketCount == 0)
+                    {
+                        displayText = "\n" + "<color=red>" + "请先连接机械臂关节数据通道" + "</color>" + "\n";
+                        flag2 = true;
+                    }
+                }
+                displayText = "\n" + "<color=black>" + "客户端 " + "</color>" + "<color=green>" + temp2.RemoteEndPoint.ToString() + "</color>" + "<color=black>" + "已连接\n" + "</color>";
                 flag2 = true;
             }
         }
@@ -104,15 +124,26 @@ namespace server
             }
             TextPrint("<color=black>" + "您当前的IP地址:" + "</color>" + "<color=blue>" + AddressIP + "</color>" + "\n");
             Debug.Log("Test");
-            IPAddress ip = IPAddress.Parse(AddressIP);
-            IPEndPoint ipe = new IPEndPoint(ip, port);
 
-            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            s.Bind(ipe);
-            s.Listen(10);
-            TextPrint("\n" + "<color=red>" + "正在等待机械臂连接......\n" + "</color>");
-            Thread thread = new Thread(ClientConnectListen);
-            thread.Start();
+            IPAddress ip1 = IPAddress.Parse(AddressIP);
+            IPEndPoint ipe1 = new IPEndPoint(ip1, port1);
+
+            IPAddress ip2 = IPAddress.Parse(AddressIP);
+            IPEndPoint ipe2 = new IPEndPoint(ip2, port2);
+
+            s1 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s1.Bind(ipe1);
+            s1.Listen(10);
+            TextPrint("\n" + "<color=red>" + "正在等待关节数据通道连接......\n" + "</color>");
+            Thread thread1 = new Thread(ClientConnectListen1);
+            thread1.Start();
+
+            s2 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s2.Bind(ipe2);
+            s2.Listen(10);
+            TextPrint("\n" + "<color=red>" + "正在等待远程控制通道连接......\n" + "</color>");
+            Thread thread2 = new Thread(ClientConnectListen2);
+            thread2.Start();
         }
 
       
@@ -139,7 +170,6 @@ namespace server
                 }
                 catch
                 {
-
                     Debug.Log("4444");
                     displayText = "\n" + "<color=red>" + "已断开，等待机械臂连接......\n" + "</color>";
                     flag2 = true;
@@ -152,40 +182,35 @@ namespace server
                 try
                 {
                     Debug.Log("开始接收控制信号");
-                    bytes3 = lst[2].Receive(recvBytes3, recvBytes3.Length, 0);
-                    recvStr2 = Encoding.UTF8.GetString(recvBytes3, 0, bytes3);
+                    bytes2 = lst[2].Receive(recvBytes2, recvBytes2.Length, 0);
+                    recvStr2 = Encoding.UTF8.GetString(recvBytes2, 0, bytes2);
                     Debug.Log("接收到："+recvStr2);
-                    /*if(recvStr2[0]=='1')
+                    if(flag5==true)
                     {
-                        byte[] bs = Encoding.UTF8.GetBytes(pose[0]+ "\r");
-                        lst[2].Send(bs, bs.Length, 0);//发送第一个角度给客户端
+                        string pos1 = Convert.ToString(pose[0]);
+                        string pos2 = Convert.ToString(pose[1]);
+                        string pos3 = Convert.ToString(pose[2]);
+                        string pos4 = Convert.ToString(pose[3]);
+                        string pos5 = Convert.ToString(pose[4]);
+                        string pos6 = Convert.ToString(pose[5]);
+                        string pos7 = Convert.ToString(pose[6]);
+                        string sendPos = pos1 + "," + pos2 + "," + pos3 + "," + pos4 + "," + pos5 + "," + pos6 + "," + pos7 + "\r";
+                        byte[] pp = Encoding.UTF8.GetBytes(sendPos);
+                        lst[2].Send(pp, pp.Length, 0);//发送位姿信息给客户端
                         Debug.Log(pose[0]);
-
-                        bs = Encoding.UTF8.GetBytes(pose[1] + "\r");
-                        lst[2].Send(bs, bs.Length, 0);//第二个
                         Debug.Log(pose[1]);
-
-                        bs = Encoding.UTF8.GetBytes(pose[2] + "\r");
-                        lst[2].Send(bs, bs.Length, 0);//第三个
                         Debug.Log(pose[2]);
-
-                        bs = Encoding.UTF8.GetBytes(pose[3] + "\r");
-                        lst[2].Send(bs, bs.Length, 0);//第四个
                         Debug.Log(pose[3]);
-
-                        bs = Encoding.UTF8.GetBytes(pose[4] + "\r");
-                        lst[2].Send(bs, bs.Length, 0);//第五个
                         Debug.Log(pose[4]);
-
-                        bs = Encoding.UTF8.GetBytes(pose[5] + "\r");
-                        lst[2].Send(bs, bs.Length, 0);//第六个
                         Debug.Log(pose[5]);
-                    }*/
+                        Debug.Log(pose[6]);
+                    }
                 }
                 catch
                 {
                     displayText = "\n" + "<color=red>" + "joint运动控制信号传输失败......\n" + "</color>";
                     flag2 = true;
+                    flag4 = false;
                 }
             }
         }
